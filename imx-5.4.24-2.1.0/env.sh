2fea4eef15e76406d3e828872f7095891502d52c
Ha Tran <hath@rikkeisoft.com>
2024-07-17 09:22:01 +0700

    feat: Add conf to build image for MX8QXPMEK.

---
+++ imx-5.4.24-2.1.0/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Here are some default settings.
+# Make sure DOCKER_WORKDIR is created and owned by current user.
+
+# Docker
+
+DOCKER_IMAGE_TAG="imx-yocto"
+DOCKER_WORKDIR="/opt/yocto"
+
+# Yocto
+
+IMX_RELEASE="imx-5.4.24-2.1.0"
+
+YOCTO_DIR="${DOCKER_WORKDIR}/${IMX_RELEASE}-build"
+
+MACHINE="imx8qxpmek"
+DISTRO="fsl-imx-xwayland"
+IMAGES="imx-image-core"
+
+REMOTE="https://github.com/hatran3e/imx-manifest"
+BRANCH="imx-linux-zeus"
+MANIFEST=${IMX_RELEASE}".xml"
